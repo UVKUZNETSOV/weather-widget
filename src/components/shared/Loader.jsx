7362c25b16a52ffcaa1f3345d438dcd32185c32c
uvkuznetsov <yuriy_kuznetsov_73@mail.ru>
2024-10-09 23:01:37 +0300

    UPD

---
+++ src/components/shared/Loader.jsx
@@ -0,0 +1,13 @@
+const Loader = () => {
+  return (
+    <div
+    className=" text-white inline-block h-32 w-32 animate-spin rounded-full border-[14px] border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+    role="status">
+    <span
+      className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+    >Loading...</span>
+  </div>
+  );
+}
+
+export default Loader;
